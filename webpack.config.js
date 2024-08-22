a5093296ddec2077a15c7c8af355bc4bf9ba92e2
Hama <pashazad@msu.edu>
2024-08-22 16:53:49 -0400

    Configure webpack to handle HTML files with html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -23,9 +23,13 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
-      },      
+      },
     ],
   },
 };
